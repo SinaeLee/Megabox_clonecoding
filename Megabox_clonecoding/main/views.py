86a03c62ea22dd59f1861c2fcbef6f4124c831a8
Sinae <sinsinae33@gmail.com>
2022-02-21 17:58:32 +0900

    정보페이지 이미지 불러오기
    info.html 이미지 불러오기

--- Megabox_clonecoding/main/views.py
+++ Megabox_clonecoding/main/views.py
@@ -2,28 +2,28 @@ from django.shortcuts import render
 from django.templatetags.static import static
 # Create your views here.
 def main_ind(request) :
-    movie = test.objects.all()
+    movie = Movie.objects.all()
     content = {'movie': movie}
     return render(request, 'main/index.html', content)
 
 
 from .models import *
-
+import csv
 def info(request, movie_id) :
-    # path = static('movie.csv')
-    # file = open(path)
     # file = open('C:\PythonStudy\Megabox_clonecoding\Megabox_clonecoding\main\movie.csv')
     # reader = csv.reader(file)
     #
     # list = []
     # for row in reader:
-    #     list.append(test(movie_type=row[0], movie_title=row[1], movie_title_eng=row[2],
+    #     list.append(Movie(movie_type=row[0], movie_title=row[1], movie_title_eng=row[2],
     #                      movie_like=row[3], movie_score=row[4], movie_rank=row[5],
     #                      movie_rate=row[6], movie_audience=row[7], movie_age=row[8],
     #                       movie_summary=row[9], movie_type2=row[10], movie_line1=row[11],
     #                       movie_line2=row[12], movie_line3=row[13], movie_line4=row[14],
-    #                       movie_line5=row[15], movie_actor=row[16]))
-    # test.objects.bulk_create(list) # 이런식으로 리스트를 한번에 넣는다.
-    movie = test.objects.get(id=movie_id)
+    #                       movie_line5=row[15], movie_actor=row[16],movie_url_bg_poster=row[17],
+    #                       movie_url_poster=row[18],movie_graph01=row[19],movie_graph03=row[20],
+    #                       movie_graph04=row[21]))
+    # Movie.objects.bulk_create(list) # 이런식으로 리스트를 한번에 넣는다.
+    movie = Movie.objects.get(id=movie_id)
     content = {'movie': movie}
     return render(request, 'main/info.html', content)
